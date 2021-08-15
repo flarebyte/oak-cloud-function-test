517162048eea4dff43e311d778c1cced29a0b391
Olivier Huin <olih@users.noreply.github.com>
2021-08-15 16:28:40 +0100

    Attemp at update field

--- src/obj-mutator.ts
+++ src/obj-mutator.ts
@@ -77,25 +77,73 @@ export const mutateObject = (rules: OakObjFieldMutation[]) => (
   return newValue;
 };
 
-
 export const findFieldValue = (path: string, value: object): any => {
-    const [first, ...rest] = path.split('.')
-    const objValue: { [name: string]:any } = value
-    const second = objValue[first]
-    if (rest.length === 0 || typeof second !== 'object') {
-        return second;
-    } else {
-        return findFieldValue(rest.join('.'), second)
-    }
+  const [first, ...rest] = path.split('.');
+  const objValue: { [name: string]: any } = value;
+  const second = objValue[first];
+  if (rest.length === 0 || typeof second !== 'object') {
+    return second;
+  } else {
+    return findFieldValue(rest.join('.'), second);
+  }
 };
 
-export const transformFieldValue = (path: string, rule: OakObjFieldMutationRule, value: object): any => {
-    const [first, ...rest] = path.split('.')
-    const objValue: { [name: string]:any } = value
-    const second = objValue[first]
-    if (rest.length === 0 || typeof second !== 'object') {
-        return second;
-    } else {
-        return findFieldValue(rest.join('.'), second)
-    }
+const getParentPath = (path: string): string => {
+  const [_last, ...rest] = path.split('.').reverse();
+  return rest ? rest.join('.') : '';
+};
+
+const getKeyOfPath = (path: string): string => {
+  const [last, ..._rest] = path.split('.').reverse();
+  return last;
+};
+
+const getPathAndParents = (
+  currentPath: string,
+  found: string[] = []
+): string[] =>
+  currentPath.includes('.')
+    ? getPathAndParents(getParentPath(currentPath), [currentPath, ...found])
+    : [currentPath, ...found];
+
+const copyObjField = (
+  path: string,
+  newValue: any,
+  content: { [key: string]: any }
+): { [key: string]: any } => {
+  const key = getKeyOfPath(path);
+  return Object.fromEntries(
+    Object.entries(content).map(keyValue =>
+      key === keyValue[0] ? [key, newValue] : keyValue
+    )
+  );
+};
+
+type TmpStackPath = { key: string; obj: { [key: string]: any } };
+
+const splitAlongPath = (
+  path: string,
+  content: { [key: string]: any }
+): TmpStackPath[] => {
+  const paths = getPathAndParents(path);
+  return paths.map(p => ({ key: p, obj: findFieldValue(p, content) }));
+};
+
+const mergeTwoPathStack = (a: TmpStackPath, b: TmpStackPath): TmpStackPath => ({
+  key: b.key,
+  obj: copyObjField(a.key, a.obj, b.obj),
+});
+const mergeAlongPath = (
+  stack: { key: string; obj: { [key: string]: any } }[]
+): { [key: string]: any } => stack.reduce(mergeTwoPathStack);
+
+export const transformFieldValue = (
+  path: string,
+  rule: OakObjFieldMutationRule,
+  content: { [key: string]: any }
+): { [key: string]: any } => {
+  const [first, ...rest] = splitAlongPath(path, content);
+  const updatedStack = [{ key: first.key, obj: rule(first.obj) }, ...rest];
+  const result = mergeAlongPath(updatedStack);
+  return result;
 };
--- test/obj-mutator.test.ts
+++ test/obj-mutator.test.ts
@@ -1,4 +1,9 @@
-import { findFieldValue, mutateObject, mutatorRules, transformFieldValue } from '../src/obj-mutator';
+import {
+  findFieldValue,
+  mutateObject,
+  mutatorRules,
+  transformFieldValue,
+} from '../src/obj-mutator';
 
 describe('Object Mutator', () => {
   describe('mutateObject', () => {
@@ -8,7 +13,9 @@ describe('Object Mutator', () => {
         kind: 'string',
         mutationName: 'string => large',
       };
-      const actual: {[index: string]:any} = mutateObject(mutatorRules)(mutation)({
+      const actual: { [index: string]: any } = mutateObject(mutatorRules)(
+        mutation
+      )({
         name: 'Picasso',
         firstName: 'Pablo',
       });
@@ -19,79 +26,76 @@ describe('Object Mutator', () => {
   });
   describe('findFieldValue', () => {
     const asset = {
-        name: 'value-of-name',
-        child: {
-            name: 'child name',
-            sizes: [12, 15],
-            siblings: [
-                { name: 'paul'}, 
-                { name: 'joe'},
-            ],
-            game: {
-                description: 'fencing'
-            }
-        }
+      name: 'value-of-name',
+      child: {
+        name: 'child name',
+        sizes: [12, 15],
+        siblings: [{ name: 'paul' }, { name: 'joe' }],
+        game: {
+          description: 'fencing',
+        },
+      },
     };
 
-      it('read field at the root', ()=> {
-        const actual = findFieldValue('name', asset)
-        expect(actual).toEqual(asset.name)
-      });
-      it('read field at the first child', ()=> {
-        const actual = findFieldValue('child.name', asset)
-        expect(actual).toEqual(asset.child.name)
-      });
-      it('read field with several levels', ()=> {
-        const actual = findFieldValue('child.game.description', asset)
-        expect(actual).toEqual(asset.child.game.description)
-      });
-      it('read field with array of integer', ()=> {
-        const actual = findFieldValue('child.sizes', asset)
-        expect(actual).toEqual(asset.child.sizes)
-      });
-      it('read field with array of obj', ()=> {
-        const actual = findFieldValue('child.siblings', asset)
-        expect(actual).toEqual(asset.child.siblings)
-      });
-      it('read first number element of array', ()=> {
-        const actual = findFieldValue('child.sizes.0', asset)
-        expect(actual).toEqual(asset.child.sizes[0])
-      });
-      it('read first object of array', ()=> {
-        const actual = findFieldValue('child.siblings.0', asset)
-        expect(actual).toEqual(asset.child.siblings[0])
-      });
+    it('read field at the root', () => {
+      const actual = findFieldValue('name', asset);
+      expect(actual).toEqual(asset.name);
+    });
+    it('read field at the first child', () => {
+      const actual = findFieldValue('child.name', asset);
+      expect(actual).toEqual(asset.child.name);
+    });
+    it('read field with several levels', () => {
+      const actual = findFieldValue('child.game.description', asset);
+      expect(actual).toEqual(asset.child.game.description);
+    });
+    it('read field with array of integer', () => {
+      const actual = findFieldValue('child.sizes', asset);
+      expect(actual).toEqual(asset.child.sizes);
+    });
+    it('read field with array of obj', () => {
+      const actual = findFieldValue('child.siblings', asset);
+      expect(actual).toEqual(asset.child.siblings);
+    });
+    it('read first number element of array', () => {
+      const actual = findFieldValue('child.sizes.0', asset);
+      expect(actual).toEqual(asset.child.sizes[0]);
+    });
+    it('read first object of array', () => {
+      const actual = findFieldValue('child.siblings.0', asset);
+      expect(actual).toEqual(asset.child.siblings[0]);
+    });
   });
   describe('transformFieldValue', () => {
     const asset = {
-        name: 'value-of-name',
-        child: {
-            name: 'child name',
-            sizes: [12, 15],
-            siblings: [
-                { name: 'paul'}, 
-                { name: 'joe'},
-            ],
-            game: {
-                description: 'fencing'
-            }
-        }
+      name: 'value-of-name',
+      child: {
+        name: 'child name',
+        sizes: [12, 15],
+        siblings: [{ name: 'paul' }, { name: 'joe' }],
+        game: {
+          description: 'fencing',
+        },
+      },
     };
 
-    const transfString = (value: string) => value + '_'
+    const transfString = (value: string) => value + '_';
 
-      it('transform field at the root', ()=> {
-        const actual = transformFieldValue('name', transfString, asset)
-        expect(actual).toEqual(asset.name + '_')
-      });
-      it('transform field at the first child', ()=> {
-        const actual = transformFieldValue('child.name',transfString, asset)
-        expect(actual).toEqual(asset.child.name+ '_')
-      });
-      it('transform field with several levels', ()=> {
-        const actual = transformFieldValue('child.game.description', transfString, asset)
-        expect(actual).toEqual(asset.child.game.description+ '_')
-      });
-
+    it('transform field at the root', () => {
+      const actual = transformFieldValue('name', transfString, asset);
+      expect(actual).toEqual(asset.name + '_');
+    });
+    it('transform field at the first child', () => {
+      const actual = transformFieldValue('child.name', transfString, asset);
+      expect(actual).toEqual(asset.child.name + '_');
+    });
+    it('transform field with several levels', () => {
+      const actual = transformFieldValue(
+        'child.game.description',
+        transfString,
+        asset
+      );
+      expect(actual).toEqual(asset.child.game.description + '_');
+    });
   });
 });
